4b0cd60b482068620f02e592e3ebe585356c6c04
Richard Myatt <lydgate77@hotmail.co.uk>
2019-05-25 22:22:25 +0100

    game now working. reset button to be added plus a bit of a tidy up

--- js/lights_out.js
+++ js/lights_out.js
@@ -12,39 +12,25 @@ var Cell = {
 };
 
 // main Vue.js application
-var main = new Vue ({
+var vm = new Vue ({
   el: "#logame",
   data: {
     title: "Lights Out Game",
     rows: 10,
     cols: 10,
-    grid: {}
+    grid: {},
+    i: 1,
+    j: 1
   },
   components: {
     locell: Cell
   },
   methods: {
     selectCell: function(i) {
-      //alert("Cell selected" + i);
-      var value = this.grid.arr[i];
-      this.$set(this.grid.arr, i, !value);
-    },
-    toggleNeighboursOfCell: function(pos) {
-      var i = pos[0];
-      var j = pos[1];
-      if(i > 1) {
-        var value = this.grid[i-1];
-        this.$set(this.grid.arr, i, value);
-      }
-      if(i < 10) {
-
-      }
-      if(j > 1) {
-
-      }
-      if(j < 10) {
-        
-      }
+      var currentPosition = this.grid.indexToPos(i);
+      this.i = currentPosition[0];
+      this.j = currentPosition[1];
+      toggleNeighboursOfCell(this.i, this.j);
     }
   },
   // initialize the app
@@ -53,3 +39,25 @@ var main = new Vue ({
     this.grid = newGrid;
   }
 });
+
+
+function toggleNeighboursOfCell(i, j) {
+  if(i > 1) {
+    toggleCell((i-1), j);
+  }
+  if(i < 10) {
+    toggleCell((i+1), j);
+  }
+  if(j > 1) {
+    toggleCell(i, (j-1));
+  }
+  if(j < 10) {
+    toggleCell(i, (j+1));
+  }
+}
+
+function toggleCell(i, j) {
+  var index = vm.grid.posToIndex([i, j]);
+  var value = vm.grid.arr[index];
+  vm.$set(vm.grid.arr, index, !value);
+}
